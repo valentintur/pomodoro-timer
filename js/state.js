a3824c9b41b509e3f9a26b2e999978baa2be2154
MacBook <macbook@MacBook-Pro-MacBook.local>
2023-02-01 20:15:22 +0100

    feat: add pause/stop functionality

--- js/state.js
+++ js/state.js
@@ -1,6 +1,13 @@
-const WORK_TIME = 25;
+const WORK_TIME = 1;
+const BREAK = 1;
+const RELAX = 1;
 
 export const state = {
+    work: WORK_TIME,
+    break: BREAK,
+    relax: RELAX ,
+    status: 'work',
+    count: 4,
     timeLeft: WORK_TIME * 60,
     isActive: false,
     timerId: 0,
--- js/timer.js
+++ js/timer.js
@@ -1,18 +1,20 @@
 import { state } from './state.js';
 import { alarm } from './alarm.js';
 import { addZero } from './util.js';
+import { changeActiveBtn } from './control.js';
 
 const minutesElem = document.querySelector('.time__minutes');
 const secondsElem = document.querySelector('.time__seconds');
 
-const showTime = (seconds) => {
+export const showTime = (seconds) => {
     minutesElem.textContent = addZero(Math.floor(seconds / 60));
     secondsElem.textContent = addZero(seconds % 60);
 }
 
 export const startTimer = () => {
-    state.timeLeft--;
-    
+    // state.timeLeft--;
+    state.timeLeft -= 20; //!!!!!!!! test 1 minete = 3 * 20 !!!!!
+
     // show on page
     showTime(state.timeLeft);
 
@@ -20,7 +22,26 @@ export const startTimer = () => {
         state.timerId = setTimeout(startTimer, 1000);
     }
 
-    if (state.timeLeft <= 0 ) {
-        alarm()
+    if (state.timeLeft <= 0) {
+        
+
+        console.log(state.activeTodo)
+        if (state.status === 'work') {
+            state.activeTodo.pomodoro += 1;
+
+            if (state.activeTodo.pomodoro % state.count) {
+                state.status = 'break'
+            } else {
+                state.status = 'relax'
+            }
+
+        } else {
+            state.status = 'work';
+        }
+
+        alarm();
+        state.timeLeft = state[state.status] * 60;
+        changeActiveBtn(state.status)
+        startTimer()
     }
 }
